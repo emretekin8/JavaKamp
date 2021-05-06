ab74aa75579ed1acac976274947cb2ef96a6c6cd
emretekin8 <emretekin601@gmail.com>
2021-05-06 05:05:11 +0300

    a

--- gameSystemDemo/src/gameSystemDemo/Main.java
+++ gameSystemDemo/src/gameSystemDemo/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
 	public static void main(String[] args) {
 //managers..
+//aaa
 		PlayerManager playerManager = new PlayerManager();
 		MarketManager gameManager = new GameManager();
 		MarketManager campaignManager = new CampaignManager();
